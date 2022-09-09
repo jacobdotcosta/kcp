ecfbeeff44fdef4c7c31373f8d7700ee45ad415d
Charles Moulliard <cmoulliard@redhat.com>
2022-09-09 16:23:33 +0200

    Created the end-to-end scenario1

---
+++ scenario-1.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#
+# End to end scenario 1
+#
+./kcp.sh clean
+./kcp.sh install -v 0.8.2
+./kcp.sh start
+
+tail -f _tmp/kcp-output.log | while read LOGLINE
+do
+   [[ "${LOGLINE}" == *"finished bootstrapping the shard workspace"* ]] && pkill -P $$ tail
+done
+echo "KCP is started :-)"
+
+kind delete cluster
+kind create cluster
+
+./kcp.sh syncer -w my-org
+./demo.sh s1
+
+
+
+
