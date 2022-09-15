06b46baf909bad943aa5f83db4d00089b153cf93
Auri Munoz <amunozhe@redhat.com>
2022-09-15 14:48:59 +0200

    fix: add config to target the good ws kcp

--- scenario-2.sh
+++ scenario-2.sh
@@ -39,25 +39,27 @@ KUBECONFIG=_tmp/.kcp/admin.kubeconfig k apply -f ./k8s
 
 note "Moving to the root:${KCP_WORKSPACE} workspace"
 note ">> k kcp ws use root:${KCP_WORKSPACE}"
-k kcp ws use root:${KCP_WORKSPACE}
+KUBECONFIG=_tmp/.kcp/admin.kubeconfig k kcp ws use root:${KCP_WORKSPACE}
 
 note "Create a quarkus app within the workspace: ${KCP_WORKSPACE}"
 note ">> k create deployment quarkus --image=quay.io/rhdevelopers/quarkus-demo:v1"
-k create deployment quarkus --image=quay.io/rhdevelopers/quarkus-demo:v1
+KUBECONFIG=_tmp/.kcp/admin.kubeconfig k create deployment quarkus --image=quay.io/rhdevelopers/quarkus-demo:v1
 note ">> k rollout status deployment/quarkus"
-k rollout status deployment/quarkus
+KUBECONFIG=_tmp/.kcp/admin.kubeconfig k rollout status deployment/quarkus
 
-note "Check deployments available within the: $(k kcp workspace .)."
+note "Check deployments available within the: $(KUBECONFIG=_tmp/.kcp/admin.kubeconfig k kcp workspace .)."
 note ">> k get deployments"
-k get deployments
+KUBECONFIG=_tmp/.kcp/admin.kubeconfig k get deployments
 
 KUBECONFIG=${KUBE_CFG} k ctx kind-cluster1
+note "Current ws is: $(KUBECONFIG=${KUBE_CFG} k kcp workspace .)"
 quarkus_pod=$(KUBECONFIG=${KUBE_CFG} k get po -lapp=quarkus -A -o name)
 if [[ $quarkus_pod == pod* ]]; then
   ((counter+=1))
 fi
 
 KUBECONFIG=${KUBE_CFG} k ctx kind-cluster2
+note "Current ws is: $(KUBECONFIG=${KUBE_CFG} k kcp workspace .)"
 quarkus_pod=$(KUBECONFIG=${KUBE_CFG} k get po -lapp=quarkus -A -o name)
 if [[ $quarkus_pod == pod* ]]; then
   ((counter+=1))
