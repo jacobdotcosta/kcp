a27fa0cc4282094ef1cea95e39987340571f442a
Charles Moulliard <cmoulliard@redhat.com>
2022-09-16 10:12:57 +0200

    refactor the dummy.sh script

--- dummy-demo.sh
+++ dummy-demo.sh
@@ -6,16 +6,16 @@ DEMO_PROMPT=">$ "
 
 TYPE_SPEED=15
 NO_WAIT=true
-p "Scenario 1: Steps executed automatically as NO_WAIT=true"
-pe "echo printing and executing automatically a \"<cmd>\""
+p "Scenario 1: Step executed automatically as NO_WAIT=true"
+pe "echo printing a \"<cmd>\""
 
-NO_WAIT=true
+p "Scenario 2: Step executed automatically but slowing the display of the text"
 TYPE_SPEED=7
-p "Scenario 2: Steps executed automatically but slowing the display of the text"
-pe "echo printing and executing automatically a \"<cmd>\""
+pe "echo printing a \"<cmd>\""
 
-p "Scenario 3: Steps executed manually as NO_WAIT=false BUT it is here needed that the user is pressing <ENTER> key"
+TYPE_SPEED=15
+p "Scenario 3: Step executed manually as NO_WAIT=false BUT it is here needed that the user is pressing <ENTER> key"
 NO_WAIT=false
-pe "echo printing and executing a \"<cmd>\" after <ENTER> key pressed"
+pe "echo printing a \"<cmd>\" after \"<ENTER>\" key pressed"
 
 clear
