66b1c46432a2e5e024fede538506714246ebd446
Charles Moulliard <cmoulliard@redhat.com>
2022-09-16 12:50:49 +0200

    Skupper end-to-end getting started is working on kind cluster

--- skupper/scenario-1.sh
+++ skupper/scenario-1.sh
@@ -4,15 +4,19 @@
 # End to end scenario to test skupper with a kind k8s cluster
 #
 
+# Scripts parameters
 : ${KIND_CLUSTER_NAME:="skupper"}
 : ${K8S_VERSION:=1.20}
 : ${HOSTNAME:=1.1.1.1.sslip.io}
+
+# Parameters to play the scenario
+TYPE_SPEED=30
+NO_WAIT=true
 
 SKUPPER_DIR="$(cd $(dirname "${BASH_SOURCE}") && pwd)"
 
 . ${SKUPPER_DIR}/../common.sh
 . ${SKUPPER_DIR}/../play-demo.sh
-
 
 if ! command -v skupper &> /dev/null; then
   warn "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
@@ -49,8 +53,8 @@ nodes:
 EOF
 )
 
-note "Deleting the kind ${KIND_CLUSTER_NAME} cluster"
-kind delete cluster --name ${KIND_CLUSTER_NAME}
+p "Deleting the kind ${KIND_CLUSTER_NAME} cluster"
+pe "kind delete cluster --name ${KIND_CLUSTER_NAME}"
 
 kindCmd="kind create cluster"
 if [ ${K8S_VERSION} == "latest" ]; then
@@ -61,41 +65,53 @@ else
   kindCmd+=" --image ${kind_image_sha}"
 fi
 
+p "Creating a Kind cluster using the kindest/node image: ${K8S_VERSION}"
 warn "############################################################################################################"
 warn "It is needed to create a k8s >=1.20 as the current version of skupper do not yet support the new ingress API"
 warn "############################################################################################################"
-
-note "Creating a Kind cluster using the kindest/node image: ${K8S_VERSION}"
 echo "${kindCfg}" | ${kindCmd} --config=- --name ${KIND_CLUSTER_NAME}
 
-note "Installing nginx ingress controller using as extra option --enable-ssl-passthrough"
-helm upgrade --install ingress-nginx ingress-nginx \
+p "Installing nginx ingress controller using as extra option --enable-ssl-passthrough"
+pe "helm upgrade --install ingress-nginx ingress-nginx \
   --repo https://kubernetes.github.io/ingress-nginx \
   --namespace ingress --create-namespace \
   --set controller.service.type=NodePort \
   --set controller.extraArgs.enable-ssl-passthrough= \
-  --set controller.hostPort.enabled=true
+  --set controller.hostPort.enabled=true"
 
-note "Wait till ingress rollout succeeded"
-k rollout status deployment/ingress-nginx-controller -n ingress
-
-# Parameters to play the scenario
-TYPE_SPEED=25
-NO_WAIT=true
+pe "k rollout status deployment/ingress-nginx-controller -n ingress"
 
 p "Initializing skupper demo scenario ..."
 NAMESPACE1="west"
 NAMESPACE2="east"
 
-pe "k create ns west"
+pe "k create ns ${NAMESPACE1}"
 pe "k config set-context --current --namespace ${NAMESPACE1}"
 pe "skupper init --ingress nginx-ingress-v1 --ingress-host ${HOSTNAME}"
 
-pe "k rollout deployment skupper-service-controller -n ${NAMESPACE1}"
-pe "k rollout deployment skupper-router -n ${NAMESPACE1}"
+pe "k rollout status deployment skupper-service-controller -n ${NAMESPACE1}"
+pe "k rollout status deployment skupper-router -n ${NAMESPACE1}"
 pe "skupper status"
 
+skupper_pwd=$(k get secret/skupper-console-users -o json | jq -r '.data.admin' | base64 -d)
+note "Skupper admin console password is: ${skupper_pwd}"
+
 pe "skupper token create ~/west.token"
-pe "k create deployment/frontend --image quay.io/skupper/hello-world-frontend"
-pe "k expose deployment/frontend --port 8080 --type ClusterIP"
-pe "k create ingress frontend --class=nginx --rule=\"frontend.${HOSTNAME}/*=quarkus:80\""
+pe "k create deployment frontend --image quay.io/skupper/hello-world-frontend"
+pe "k expose deployment frontend --port 8080 --type ClusterIP"
+pe "k create ingress frontend --class=nginx --rule=\"frontend.${HOSTNAME}/*=frontend:80\""
+
+pe "k create ns ${NAMESPACE2}"
+pe "kubectl config set-context --current --namespace ${NAMESPACE2}"
+pe "skupper init --ingress none"
+
+pe "k rollout status deployment skupper-service-controller -n ${NAMESPACE2}"
+pe "k rollout status deployment skupper-router -n ${NAMESPACE2}"
+pe "skupper status"
+
+pe "skupper link create ~/west.token"
+pe "k create deployment backend --image quay.io/skupper/hello-world-backend --replicas 3"
+pe "skupper expose deployment/backend --port 8080"
+
+succeeded "Frontend is available using this url in your browser: frontend.${HOSTNAME}"
+
